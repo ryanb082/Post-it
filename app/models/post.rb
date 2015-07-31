37b81142fa91ff239ea0b8731769dfcf8e706983
Ryan Bent <ryanbent01@gmail.com>
2015-07-31 23:22:53 +0000

    Fix NameError when in irb

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :votes, as: :voteable
 
+
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true
   validates :url, presence: true, uniqueness: true
+
+  before_save :generate_slug
 
   def total_votes
   	 self.up_votes - self.down_votes
@@ -19,5 +22,10 @@ class Post < ActiveRecord::Base
 
   def down_votes
   	self.votes.where(vote: false).size
+  end
+
+  def generate_slug
+    self.slug = self.title.gsub(" ", "-").downcase
+
   end
 end
