2e0b1f691fdbb6123d8ef74669e7dde85dfa2168
Ryan Bent <ryanbent01@gmail.com>
2015-08-04 20:32:29 +0000

    Add slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
   validates :url, presence: true
   validates :url, presence: true, uniqueness: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def total_votes
   	 self.up_votes - self.down_votes
@@ -28,7 +28,30 @@ class Post < ActiveRecord::Base
     self.slug
   end
 
-  def generate_slug
-    self.slug = self.title.gsub(" ", "-").downcase
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+     the_slug = append_suffix(the_slug, count)
+     post = Post.find_by slug: the_slug
+     count += 1
+    end
+    self.slug = the_slug.downcase
   end
-end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!  /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub!  /-+/, "-"
+    str.downcase
+  end
+end  
