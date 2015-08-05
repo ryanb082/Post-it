6a9f615aa63374ebe83df2861eef8d81a1df8699
Ryan Bent <ryanbent01@gmail.com>
2015-08-05 00:09:46 +0000

    fix two_factor authentication error in views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804231737) do
+ActiveRecord::Schema.define(version: 20150804235313) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20150804231737) do
     t.string   "password_digest"
     t.string   "slug"
     t.string   "role"
+    t.string   "pin"
+    t.string   "phone"
   end
 
   create_table "votes", force: true do |t|
